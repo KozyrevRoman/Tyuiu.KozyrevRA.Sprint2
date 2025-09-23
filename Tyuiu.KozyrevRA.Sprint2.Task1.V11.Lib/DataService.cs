2fd1c9a4deb750f80e019e0c31f39103a143d233
Роман Козырев <Tyuiu.KozyrevRA@outlook.com>
2025-09-23 21:24:35 +0500

    Task1

---
+++ Tyuiu.KozyrevRA.Sprint2.Task1.V11.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.KozyrevRA.Sprint2.Task1.V11.Lib
+{
+    public class DataService : ISprint2Task1V11
+    {
+        public bool[] GetLogicOperations(int a, int b, int c, int d)
+        {
+            bool[] res = new bool[6];
+            res[0] = (a>b)|(c<d);
+            res[1] = (a+11!=b)&(c<d);
+            res[2] = (a+11>=b)||(c>=d);
+            res[3] = (a!=b+3)&&(c==d);
+            res[4] = !(a > b) == (c >= d);
+            res[5] = (a > b + 70) ^ (c < d);
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.KozyrevRA.Sprint2.Task1.V11/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KozyrevRA.Sprint2.Task1.V11.Lib;
+namespace Tyuiu.KozyrevRA.Sprint2.Task1.V11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            int a = 145;
+            int b = 156;
+            int c = 142;
+            int d = 117;
+            bool[] res = new bool[6];
+            res = ds.GetLogicOperations(a, b, c, d);
+            Console.Title = "Спринт #2 | Выполнил: Козырев Р. А. | СМАРТБ-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема:  Логические операции                                              *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #11                                                             *");
+            Console.WriteLine("* Выполнил: Козырев Роман Артёмович | СМАРТБ-25-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу из операций сравнений (==, !=, <, >, <=, >=,         *");
+            Console.WriteLine("* последовательность можно чередовать, но использовать один раз в         *");
+            Console.WriteLine("* выражении) и логических операций(|, &, ||, &&, !, ^, последовательность *");
+            Console.WriteLine("* операций не должна нарушаться), а также арифметических выражений,       *");
+            Console.WriteLine("* которая вернет логическую последовательность(массив):                   *");  
+            Console.WriteLine("* (False, False, True, False, True, False), при                           *");
+            Console.WriteLine("* a = 145, b = 156, c = 142, d = 117                                      *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine(" a = " + a);
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine(" b = " + b);
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine(" c = " + c);
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine(" d = " + d);
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(res[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
