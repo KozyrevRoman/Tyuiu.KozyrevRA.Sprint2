cdc8c614079d7792c59c2ee25b4380176930dc93
Роман Козырев <Tyuiu.KozyrevRA@outlook.com>
2025-09-26 18:28:25 +0500

    Task4

---
+++ Tyuiu.KozyrevRA.Sprint2.Task4.V28.Test/DataServiceTest.cs
@@ -0,0 +1,28 @@
+﻿using Tyuiu.KozyrevRA.Sprint2.Task4.V28.Lib;
+namespace Tyuiu.KozyrevRA.Sprint2.Task4.V28.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCondition1()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            double y = 10;
+            double res = ds.Calculate(x, y);
+            double wait =13.0;
+            Assert.AreEqual(wait, res);
+        }
+        [TestMethod]
+        public void ValidCondition2()
+        {
+            DataService ds = new DataService();
+            double x = 10;
+            double y = 1;
+            double res = ds.Calculate(x, y);
+            double wait = 20.150;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.KozyrevRA.Sprint2.Task4.V28/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KozyrevRA.Sprint2.Task4.V28.Lib;
+namespace Tyuiu.KozyrevRA.Sprint2.Task4.V28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #2 | Выполнил: Козырев Р. А. | СМАРТБ-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема:  Тернарный оператор                                               *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #28                                                             *");
+            Console.WriteLine("* Выполнил: Козырев Роман Артёмович | СМАРТБ-25-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет требуемое значение с              *");
+            Console.WriteLine("* использованием тернарного, где пользователь вводит значение             *");
+            Console.WriteLine("* переменной x,y с клавиатуры, если x < y * 2 - 10 , то                   *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("*      (    y + 2  )^x         2x^2 - cos(y)^2 + 12                       *");
+            Console.WriteLine("* z = ( 1 + -----   )  иначе  ----------------------                      *");
+            Console.WriteLine("*      (     x^2  )            y^2 - sin(x)^2 +9                          *");
+            Console.WriteLine("* Ответ округлите до 3 знаков после запятой.                              *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("Введите значение переменной X: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите значение переменной Y: ");
+            int y = Convert.ToInt32(Console.ReadLine());
+            double res = ds.Calculate(x, y);
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Значение функции = " + res);
+            Console.ReadKey();
+        }
+    }
+}
